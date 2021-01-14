1315f916999d146b295f3fb4c953c9def31d1ec5
saddamarbaa <saddamarbaas@gmail.com>
2021-01-15 02:00:34 +0700

    added Selectors(js file)

---
+++ app.js
@@ -0,0 +1,18 @@
+//  Selectors
+
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// console.log(todoInput);
+// console.log(todoButton);
+// console.log(todoInput);
+
+//  Event Listener
+alert(1);
+todoButton.addEventListener("onclick", addTodo);
+
+// Functions
+function addTodo(event) {
+  // event.preventDefault();
+}
