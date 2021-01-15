b1278e49398623b1ceeb3d5d83239a77694bff54
saddamarbaa <saddamarbaas@gmail.com>
2021-01-15 09:39:24 +0700

    added addTodo function

--- app.js
+++ app.js
@@ -9,10 +9,40 @@ const todoList = document.querySelector(".todo-list");
 // console.log(todoInput);
 
 //  Event Listener
-alert(1);
-todoButton.addEventListener("onclick", addTodo);
+
+todoButton.addEventListener("click", addTodo);
 
 // Functions
+
 function addTodo(event) {
-  // event.preventDefault();
+  // Prevent form from Submiting
+  event.preventDefault();
+
+  // Todo Div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  // Create LI
+  const newTodo = document.createElement("li");
+  newTodo.innerHTML = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  // CREATE CHECK MARK BUTTON
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  // CREATE Trash  BUTTON
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+
+  // APPEND TO LIST
+  todoList.appendChild(todoDiv);
+
+  // Clear Todo Input Value
+  todoInput.value = "";
 }
